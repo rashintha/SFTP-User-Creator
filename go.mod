b110864916af3a892469e6aaf46c947a973e23f8
Rashintha Maduneth <rashinthamaduneth@gmail.com>
2023-05-28 23:56:42 +0530

    Google auth and document creating developed.

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.3 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
 	github.com/xuri/efp v0.0.0-20230422071738-01f4e37c47e9 // indirect
 	github.com/xuri/nfp v0.0.0-20230503010013-3f38cdbb0b83 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/rashintha/logger"
+	"github.com/skip2/go-qrcode"
 	"github.com/xuri/excelize/v2"
 )
 
@@ -36,7 +41,7 @@ func main() {
 		logger.ErrorFatal(err.Error())
 	}
 
-	logger.Defaultln("Validating the content.")
+	logger.Defaultln("Validating the file content.")
 	if rows[0][0] != "Username" || rows[0][1] != "Password" {
 		logger.ErrorFatal("Header row values are invalid.")
 	}
@@ -98,8 +103,157 @@ func main() {
 			createDir(dirRow[0], row[0])
 		}
 
-		// Print the output
-		fmt.Println(strings.TrimSuffix(string(output), "\n"))
+		secret, _ := googleAuthenticator(row[0])
+
+		prepareDoc(row[0], row[1], secret)
+
+	}
+}
+
+func prepareDoc(user string, password string, secret string) {
+	logger.Defaultf("Preparing word document")
+
+	logger.Defaultf("Removing existing doc processing folders")
+
+	err := os.RemoveAll("doc")
+	if err != nil {
+		logger.Errorf("Failed removing directory: %s", err)
+	}
+
+	logger.Defaultf("Unzipping the sample document.")
+	cmd := exec.Command("unzip", "-d", "doc", "doc.docx")
+
+	err = cmd.Run()
+	if err != nil {
+		logger.ErrorFatalf("Unzip command failed with %s\n", err)
+	}
+
+	logger.Defaultf("Copying qr code to document")
+
+	// Open source file for reading
+	srcFile, err := os.Open(fmt.Sprintf("qr/%v.png", user))
+	if err != nil {
+		logger.ErrorFatal(err.Error())
+	}
+	defer srcFile.Close()
+
+	// Open destination file for writing
+	dstFile, err := os.Create("doc/word/media/image2.png")
+	if err != nil {
+		logger.ErrorFatal(err.Error())
+	}
+	defer dstFile.Close()
+
+	// Copy the source file to the destination file
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		logger.ErrorFatal(err.Error())
+	}
+
+	// Sync to ensure that the copy operation is complete before the program exits
+	err = dstFile.Sync()
+	if err != nil {
+		logger.ErrorFatal(err.Error())
+	}
+
+	logger.Defaultf("Updating the document")
+
+	docPath := "doc/word/document.xml"
+	// Read the file
+	content, err := ioutil.ReadFile(docPath)
+	if err != nil {
+		logger.ErrorFatalf("Error reading file: %s", err)
+	}
+
+	// Replace the phrase
+	newContent := strings.ReplaceAll(string(content), "&lt;user&gt;", user)
+	newContent = strings.ReplaceAll(string(newContent), "&lt;pass&gt;", password)
+	newContent = strings.ReplaceAll(string(newContent), "&lt;secret&gt;", secret)
+
+	// Write the new content back to the file
+	err = ioutil.WriteFile(docPath, []byte(newContent), 0644)
+	if err != nil {
+		logger.ErrorFatalf("Error writing file: %s", err)
+	}
+
+	logger.Defaultf("Creating documents directory if not exists")
+	cmd = exec.Command("sudo", "mkdir", "-p", "documents") // replace "ls -l" with your command
+
+	// This will capture the output from the command
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		logger.Errorln(strings.TrimSuffix(string(output), "\n"))
+		logger.ErrorFatal(err.Error())
+	}
+
+	logger.Defaultf("Creating the updated document")
+
+	// Create a file to write our archive to.
+	file, err := os.Create(fmt.Sprintf("documents/%v.docx", user))
+	if err != nil {
+		logger.ErrorFatalf("Error creating docx file: %s", err)
+	}
+	defer file.Close()
+
+	// Create a new zip archive.
+	w := zip.NewWriter(file)
+
+	// Add files to zip.
+	if err := filepath.Walk("doc", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Create header.
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		header.Name = strings.TrimPrefix(path, filepath.Clean("doc")+"/")
+
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			// If file is a symlink, dereference it.
+			if info.Mode()&os.ModeSymlink != 0 {
+				realPath, err := os.Readlink(path)
+				if err != nil {
+					return err
+				}
+				header.Method = zip.Deflate
+				path = realPath
+			}
+		}
+
+		// Create writer.
+		writer, err := w.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		// Write data to zip.
+		if info.Mode().IsRegular() {
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			_, err = io.Copy(writer, file)
+			file.Close()
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}); err != nil {
+		logger.ErrorFatalf("Error writing file: %s", err)
+	}
+
+	// Make sure to check the error on Close.
+	err = w.Close()
+	if err != nil {
+		logger.ErrorFatalf("Error closing writer: %s", err)
 	}
 }
 
@@ -135,4 +289,73 @@ func createDir(dir string, user string) {
 		logger.Errorln(strings.TrimSuffix(string(output), "\n"))
 		logger.ErrorFatal(err.Error())
 	}
+}
+
+func googleAuthenticator(user string) (string, []string) {
+	logger.Defaultln("Enabling Google authenticator")
+
+	cmd := exec.Command("sudo", "-u", user, "google-authenticator", "-t", "-d", "-r3", "-R30", "-f", "-C", "-w3", "-Q", "UTF8") // replace "ls -l" with your command
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		logger.Errorln(strings.TrimSuffix(string(output), "\n"))
+		logger.ErrorFatal(err.Error())
+	}
+
+	logger.Defaultf("Generating the QR Code")
+	outStr := string(output)
+	startIndex := strings.Index(outStr, "otpauth://totp/")
+	endIndex := strings.Index(outStr[startIndex:], "\n")
+
+	if startIndex == -1 || endIndex == -1 {
+		logger.ErrorFatal("Could not find URL")
+
+	}
+
+	url := outStr[startIndex : startIndex+endIndex]
+
+	var errQR error
+	var png []byte
+	png, errQR = qrcode.Encode(url, qrcode.Medium, 1024)
+	if errQR != nil {
+		logger.ErrorFatalf("Could not generate QR Code: %s\n", errQR)
+	}
+
+	logger.Defaultf("Creating qr directory if not exists")
+	cmd = exec.Command("sudo", "mkdir", "-p", "qr") // replace "ls -l" with your command
+
+	// This will capture the output from the command
+	output, err = cmd.CombinedOutput()
+	if err != nil {
+		logger.Errorln(strings.TrimSuffix(string(output), "\n"))
+		logger.ErrorFatal(err.Error())
+	}
+
+	logger.Defaultf("Saving the QR code")
+	qrFileName := fmt.Sprintf("qr/%v.png", user)
+
+	// Write the PNG to a file
+	err = ioutil.WriteFile(qrFileName, png, 0644)
+	if err != nil {
+		logger.ErrorFatalf("Could not write file: %s\n", err)
+	}
+
+	startIndex = strings.Index(outStr, "Your new secret key is: ") + 24
+	endIndex = strings.Index(outStr[startIndex:], "\n")
+
+	if startIndex == -1 || endIndex == -1 {
+		logger.ErrorFatal("Could not find the secret key.")
+
+	}
+
+	secretKey := outStr[startIndex : startIndex+endIndex]
+
+	// Find the scratch codes
+	startIndex = strings.Index(outStr, "Your emergency scratch codes are:")
+	if startIndex == -1 {
+		logger.ErrorFatal("Could not find scratch codes")
+	}
+	scratchCodes := strings.Split(outStr[startIndex:], "\n")[1:6]
+
+	return secretKey, scratchCodes
 }
